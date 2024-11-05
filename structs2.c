3a17dc0ed91398de396ee9e654542cb949f45553
mradulnatani <mradulnatni0@gmail.com>
2024-11-05 19:00:18 +0530

    structs

---
+++ structs2.c
@@ -0,0 +1,35 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+int main() {
+    WSADATA wsaData;
+    struct addrinfo hints, *res;
+    int result;
+
+    // Initialize Winsock
+    result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) {
+        printf("WSAStartup failed: %d\n", result);
+        return 1;
+    }
+
+    // Setup the hints address info structure
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    // Get address info
+    result = getaddrinfo("www.google.com", "http", &hints, &res);
+    if (result != 0) {
+        printf("getaddrinfo failed: %d\n", result);
+        WSACleanup();
+        return 1;
+    }
+
+    // Cleanup and exit
+    freeaddrinfo(res);
+    WSACleanup();
+
+    return 0;
+}
